ffa6dfd4f11bc5e8408c32c749e8faa1346715c9
Sachko-Sergey <sachkosergey11@gmail.com>
2019-11-28 17:30:03 +0300

    medirev

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 $(function () {
     $('.slider').slick({
-        prevArrow: '<button type="button" class="slick-prev  arrow-slide-right"><img src="../images/arrow-left.png" /></button>',
-        nextArrow: '<button type="button" class="slick-prev  arrow-slide-left"><img src="../images/arrow-right.png" /></button>',
+        prevArrow: '<button type="button" class="slick-prev  arrow-slide-right"><img src="images/arrow-left.png" /></button>',
+        nextArrow: '<button type="button" class="slick-prev  arrow-slide-left"><img src="images/arrow-right.png" /></button>',
     });
 
     $('input, select').styler();
